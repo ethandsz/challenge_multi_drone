384cd5b9487a744097e3414c0affae03635b92cf
Ethan D'Souza <91471122+ethandsz@users.noreply.github.com>
2025-03-14 00:21:24 +0000

    init

---
+++ build/led_ring_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ethan/Documents/Github/comp0240/challenge_multi_drone_cw/src/challenge_multi_drone/config_sim/gazebo/plugins/led_ring_plugin
+# Build directory: /home/ethan/Documents/Github/comp0240/challenge_multi_drone_cw/src/challenge_multi_drone/build/led_ring_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
